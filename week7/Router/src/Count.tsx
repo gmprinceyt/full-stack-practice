42c22697c165e2b087585a8ea6424da052f26fc0
PRINCE KUAMR <gmprince420@gmail.com>
2025-08-06 20:42:08 +0530

    context concepts'

--- week7/Router/src/Count.tsx
+++ week7/Router/src/Count.tsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from "react";
+import { CountContext } from "./Context";
 
 /**
  * ✅ Simple Definition:
  * Props drilling is when you pass data from a top-level component to a deep child component
  * by going through several layers of intermediate components that don’t use the data, just pass it down.
- * 
+ *
  */
 
 /**
  * ✅ How to Explain in Interview:
- * "Props drilling happens when we need to send data from a parent to a deep child, 
- * but we have to pass that data through many components in between. 
- * Those middle components don’t use the data—they just pass it forward. 
- * It can make the code messy and harder to manage. 
+ * "Props drilling happens when we need to send data from a parent to a deep child,
+ * but we have to pass that data through many components in between.
+ * Those middle components don’t use the data—they just pass it forward.
+ * It can make the code messy and harder to manage.
  * That’s why we use solutions like Context API, Redux, or Zustand to avoid props drilling."
  */
 
@@ -21,38 +22,54 @@ import React, { useState } from 'react'
 “Props drilling is when we pass data through many layers of components just to reach the one that actually needs it.”
  */
 
-
 const Count = () => {
-    const [Count , setCount ] = useState(0)
+  const [Count, setCount] = useState(0);
   return (
-    <div style={{
-        padding: "50px "
-    }}>
-        <Show Count={Count} setCount={setCount} />
+    <div style={{ padding: "50px " }}>
+      <CountContext.Provider value={Count}>
+        <Show setCount={setCount} />
+      </CountContext.Provider>
     </div>
-  )
+  );
+};
+
+function Show({
+  setCount,
+}: {
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+}) {
+    const Count = useContext(CountContext)
+  return (
+    <>
+      <b>{Count}</b>
+      <Buttons setCount={setCount} />
+      <Show2/>
+    </>
+  );
 }
 
-function Show({Count, setCount}:{Count: number, setCount: React.Dispatch<React.SetStateAction<number>>}){
-    return (
-        <>
-        <b>{Count}</b>
-                <Buttons setCount={setCount}/>
-
-        </>
-
-    )
+function Show2() {
+    const Count = useContext(CountContext)
+  return (
+    <>
+      <b>{Count}</b>
+    </>
+  );
 }
 
-function Buttons({setCount}: {setCount: React.Dispatch<React.SetStateAction<number>> }) {
-    return (
-        <div>
+function Buttons({
+  setCount,
+}: {
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+}) {
+        const Count = useContext(CountContext)
 
-            <button onClick={()=> setCount(p => p + 1)}>increase</button>
-            <button onClick={()=> setCount(p => p - 1)}>Decrease</button>
-        </div>
-    )
+  return (
+    <div>
+      <button onClick={() => setCount(Count + 1)}>increase</button>
+      <button onClick={() => setCount(Count - 1)}>Decrease</button>
+    </div>
+  );
 }
 
-
-export default Count
+export default Count;
--- week7/Router/src/main.tsx
+++ week7/Router/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Count from './Count.tsx'
+import {CountContext} from './Context.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Count />
+    {/* <Context/> */}
     {/* <App/> */}
   </StrictMode>
 )
