0761088847d15b0d1753edc7be06926e7fc25ba1
PRINCE KUAMR <gmprince420@gmail.com>
2025-08-30 18:19:22 +0530

    Diff functional or class based

---
+++ week-9/Custom Hooks/src/App.tsx
@@ -0,0 +1,43 @@
+import { Component, useEffect, useState, type ReactNode } from "react";
+import "./App.css";
+
+function App() {
+  const [render, setRender] = useState(true);
+
+  useEffect(() => {
+    console.log("app")
+    setInterval(() => {
+      setRender((p) => !p);
+    }, 4000);
+  }, []);
+  return <>{render ? <Mycomponent /> : <>App Component</>}</>;
+}
+
+class Mycomponent extends Component {
+  componentDidMount(): void {
+    console.log("Component mounted");
+  }
+  componentWillUnmount(): void {
+    console.log("Component unmounted");
+  }
+  render(): ReactNode {
+    return (
+      <>
+        <div>MY Component</div>
+      </>
+    );
+  }
+}
+
+function Mycomponent() {
+  useEffect(() => {
+    console.log("component Mounted");
+    return () => {
+      console.log("component unmount");
+    };
+  }, []);
+
+  return <>My Components </>;
+}
+
+export default App;
